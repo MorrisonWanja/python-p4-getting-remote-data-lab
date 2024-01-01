d480c6c34dbdee098bd91e2371ecf952ab3c96b9
morrison <morrisonwanja@gmail.com>
2024-01-01 09:14:07 +0300

    Complete Assignement

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,17 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
+        response = requests.get(self.url)
+        return response.content
         pass
 
     def load_json(self):
+        # result_list=[]
+        results =json.loads(self.get_response_body())
+        return results
         pass
+
+req = GetRequester("https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
+out = req.get_response_body()
+ind = req.load_json()
+print(ind)
